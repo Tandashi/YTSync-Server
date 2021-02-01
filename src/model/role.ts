8a71ad9c334f2557ea8bf37e5f407e46dfb1f43b
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-02-01 17:24:18 +0100

    fix: Remove old code that was kept to support older versions prior to 1.7.0
    BRACKING CHANGE: Older versions < 1.7.0 will not longer work

--- src/model/role.ts
+++ src/model/role.ts
@@ -2,9 +2,5 @@ export enum Role {
     HOST = 'host',
     SUB_HOST = 'sub-host',
     MODERATOR = 'moderator',
-    /**
-     * @deprecated in favor of {@link SUB_HOST} & {@link MODERATOR} permissions.
-     */
-    PROMOTED = 'promoted',
     MEMBER = 'member'
 }
