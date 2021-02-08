ae128670a41c34ca5e88cb20c5c723daeefa7ceb
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-02-08 14:49:11 +0100

    refactor: Formating

--- src/model/message.ts
+++ src/model/message.ts
@@ -1,34 +1,26 @@
 import logger from '../logger';
 
 export enum Message {
-    PLAY = 'play',
-    PAUSE = 'pause',
-    SEEK = 'seek',
-    AUTOPLAY = 'autoplay',
-    PLAY_VIDEO = 'play-video',
-    ADD_TO_QUEUE = 'add-to-queue',
-    REMOVE_FROM_QUEUE = 'remove-from-queue',
-    QUEUE = 'queue',
-    CLIENTS = 'clients',
-    CLIENT_CONNECT = 'client-connect',
-    CLIENT_DISCONNECT = 'client-disconnect',
-    SET_ROLE_MEMBER = 'set-role-member',
-    SET_ROLE_MODERATOR = 'set-role-moderator',
-    SET_ROLE_SUB_HOST = 'set-role-sub-host',
-    /**
-     * @deprecated in favor of {@link SUB_HOST} & {@link MODERATOR} permissions.
-     */
-    PROMOTE = 'promote',
-    /**
-     * @deprecated in favor of {@link SUB_HOST} & {@link MODERATOR} permissions.
-     */
-    UNPROMOTE = 'unpromote',
-    REACTION = 'reaction'
+  PLAY = 'play',
+  PAUSE = 'pause',
+  SEEK = 'seek',
+  AUTOPLAY = 'autoplay',
+  PLAY_VIDEO = 'play-video',
+  ADD_TO_QUEUE = 'add-to-queue',
+  REMOVE_FROM_QUEUE = 'remove-from-queue',
+  QUEUE = 'queue',
+  CLIENTS = 'clients',
+  CLIENT_CONNECT = 'client-connect',
+  CLIENT_DISCONNECT = 'client-disconnect',
+  SET_ROLE_MEMBER = 'set-role-member',
+  SET_ROLE_MODERATOR = 'set-role-moderator',
+  SET_ROLE_SUB_HOST = 'set-role-sub-host',
+  REACTION = 'reaction',
 }
 
 export enum VideoState {
-    PLAYING = 'play',
-    PAUSED = 'pause'
+  PLAYING = 'play',
+  PAUSED = 'pause',
 }
 
 /**
@@ -38,13 +30,21 @@ export enum VideoState {
  * @param type The type of the message
  * @param data The data of the message
  */
-export function sendMessageToSocket(socket: SocketIO.Socket, type: Message, data: any): void {
-    const message = {
-        action: type,
-        data
-    };
-    logger.info(`Sending Message (${JSON.stringify(message)}) to Socket -> socketId: ${socket.id}`);
-    socket.emit('message', JSON.stringify(message));
+export function sendMessageToSocket(
+  socket: SocketIO.Socket,
+  type: Message,
+  data: any
+): void {
+  const message = {
+    action: type,
+    data,
+  };
+  logger.info(
+    `Sending Message (${JSON.stringify(message)}) to Socket -> socketId: ${
+      socket.id
+    }`
+  );
+  socket.emit('message', JSON.stringify(message));
 }
 
 /**
@@ -54,13 +54,15 @@ export function sendMessageToSocket(socket: SocketIO.Socket, type: Message, data
  *
  * @return The Message type of undefined if there is no Message type the the VideoState
  */
-export function getMessageFromVideoState(state: VideoState): Message | undefined {
-    switch(state) {
-        case VideoState.PLAYING:
-            return Message.PLAY;
-        case VideoState.PAUSED:
-            return Message.PAUSE;
-    }
+export function getMessageFromVideoState(
+  state: VideoState
+): Message | undefined {
+  switch (state) {
+    case VideoState.PLAYING:
+      return Message.PLAY;
+    case VideoState.PAUSED:
+      return Message.PAUSE;
+  }
 
-    return undefined;
+  return undefined;
 }
--- src/service/name-service.ts
+++ src/service/name-service.ts
@@ -1,17 +1,21 @@
-import { uniqueNamesGenerator, Config, colors, animals } from 'unique-names-generator';
+import {
+  uniqueNamesGenerator,
+  Config,
+  colors,
+  animals,
+} from 'unique-names-generator';
 
 const customConfig: Config = {
-    dictionaries: [colors, animals],
-    separator: ' ',
-    length: 2,
+  dictionaries: [colors, animals],
+  separator: ' ',
+  length: 2,
 };
 
 export default class NameSerivce {
-
-    /**
-     * Get a random Name
-     */
-    public static getName(): string {
-        return uniqueNamesGenerator(customConfig);
-    }
+  /**
+   * Get a random Name
+   */
+  public static getName(): string {
+    return uniqueNamesGenerator(customConfig);
+  }
 }
